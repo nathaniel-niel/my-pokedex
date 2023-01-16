5c5def85a25b4916765fc788e7beeb9261d056c9
nathaniel andrian <nathaniel.task@gmail.com>
2023-01-17 00:52:36 +0700

    Fix model data

--- MyPokedex/Scenes/FavoritePokemon/Model/FavoritePokemonModel.swift
+++ MyPokedex/Scenes/FavoritePokemon/Model/FavoritePokemonModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct FavoritePokemonModel {
+    let pokemonId: Int
+    let name: String
+    let image: String
+    let types: String
+    let abilities: String
+    let base_experience: String
+    let height: String
+    let weight: String
+}
